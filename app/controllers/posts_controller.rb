ce00542b2754630aa9d38cc715814bc0b5be3b7e
Timothy Joy <tim.joy47@gmail.com>
2018-01-30 14:23:25 -0600

    added a function to the post controller to change the title of the first, and every fifth post after that to SPAM

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,15 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each do |post|
+      if post.id == 1
+        post.title = "SPAM"
+      elsif post.id % 5 == 1
+        post.title = "SPAM"
+      else post
+      end
+    p = Post.find(6)
+    puts "#{{p.title}}"  
   end
 
   def show
