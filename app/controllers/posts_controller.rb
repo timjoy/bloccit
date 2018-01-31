846a2f1a915c40f8c324411e2525d136f9294ebc
Timothy Joy <tim.joy47@gmail.com>
2018-01-30 23:19:26 -0600

    added a bunch of stuff for post new, create, and show in controller and view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,45 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-    @posts.each do |post|
-      if post.id == 1
+    # @posts.each do |post|
+      # if post.id == 1
+      #   post.title = "SPAM"
+      # elsif post.id % 5 == 1
+      #   post.title = "SPAM"
+      # else post
+    @posts.each_with_index do |post, index|
+      if index % 5 == 0
         post.title = "SPAM"
-      elsif post.id % 5 == 1
-        post.title = "SPAM"
-      else post
       end
-    p = Post.find(6)
-    puts "#{{p.title}}"  
   end
+  #   p = Post.find(6)
+  #   puts "#{p.title}"
+  # end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
   end
 
   def edit
   end
+end
 end
