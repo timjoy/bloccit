c833f81663aa6b1201e477439fcf3d7e306e5094
Timothy Joy <tim.joy47@gmail.com>
2018-02-12 09:18:46 -0600

    still the model spec error

--- spec/models/sponsored_post_spec.rb
+++ spec/models/sponsored_post_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe SponsoredPost, type: :model do
-  # let(:name) { RandomData.random_sentence }
-  # let(:description) { RandomData.random_paragraph }
-  # let(:title) { RandomData.random_sentence }
-  # let(:body) { RandomData.random_paragraph }
-  # let(:price) { RandomData.random_price}
-  # let(:topic) { Topic.create!(name: name, description: description) }
-  # let(:sponsoredpost) { topic.sponsoredposts.create!(title: title, body: body, price: price) }
-  let(:my_topic) { Topic.create!(name:  RandomData.random_sentence, description: RandomData.random_paragraph) }
-  let(:sponsoredpost) { my_topic.sponsoredposts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, price: RandomData.random_price) }
-  # it { is_expected.to belong_to(:topic) }
+  let(:name) { RandomData.random_sentence }
+  let(:description) { RandomData.random_paragraph }
+  let(:title) { RandomData.random_sentence }
+  let(:body) { RandomData.random_paragraph }
+  let(:price) { RandomData.random_price}
+  let(:topic) { Topic.create!(name: name, description: description) }
+  let(:sponsored_post) { topic.sponsored_post.create!(title: title, body: body, price: price) }
+  # let(:my_topic) { Topic.create!(name:  RandomData.random_sentence, description: RandomData.random_paragraph) }
+  # let(:sponsoredpost) { my_topic.sponsoredposts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, price: RandomData.random_price) }
+  it { is_expected.to belong_to(:topic) }
 
   describe "attributes" do
     it "has a title and body attribute" do
-      expect(sponsoredpost).to have_attributes(title: title, body: body, price: price)
+      expect(sponsored_post).to have_attributes(title: title, body: body, price: price)
     end
   end
 end
